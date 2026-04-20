ca6630ab7ff4d88fd440ff79ca45ec91aa728fb6
Francisco Zanfranceschi <zanfranceschi@gmail.com>
2026-04-20 01:08:46 +0000

    rename

--- containerization/build-and-publish.sh
+++ containerization/build-and-publish.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-IMAGE="zanfranceschi/rinha-de-backend-2026-clojure-exemplo"
+IMAGE="zanfranceschi/rinha-de-backend-2026-zan-clojure"
 TAG=$(date +%Y%m%d%H%M)
 
-docker build -t "$IMAGE:$TAG" -t "rinha-de-backend-2026-clojure-exemplo:latest" -f Dockerfile ..
+docker build -t "$IMAGE:$TAG" -t "rinha-de-backend-2026-zan-clojure:latest" -f Dockerfile ..
 docker push "$IMAGE:$TAG"
 
 echo "Published $IMAGE:$TAG"
--- containerization/build-locally.sh
+++ containerization/build-locally.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-docker build -t "rinha-de-backend-2026-clojure-exemplo:latest" -f Dockerfile ..
+docker build -t "rinha-de-backend-2026-zan-clojure:latest" -f Dockerfile ..
 
 echo "image built"
